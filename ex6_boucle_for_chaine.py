b794b7a2ce356f558b437071e942045ac4e501d8
Nels-G <nelsgalley@gmail.com>
2025-04-18 19:34:23 +0000

    exo6

---
+++ ex6_boucle_for_chaine.py
@@ -0,0 +1,9 @@
+chaine = input("Entrez une chaîne de caractères : ")
+print("Caractères de la chaîne :")
+for c in chaine:
+    print(c)
+
+liste = [1, 2, 3, 4, 5]
+print("Éléments de la liste :")
+for elem in liste:
+    print(elem)
